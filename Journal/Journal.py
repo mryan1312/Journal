efe374d11d417bb6d6254db9a2d1f58fdde96988
mryan1312 <mryan1312@outlook.com>
2023-06-17 05:36:18 -0500

    Data capture complete. SQL operations partially implemented.

--- Journal/Journal.py
+++ Journal/Journal.py
@@ -3,6 +3,7 @@ import asyncio
 import sqlite3
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
 import geocoder
 import python_weather
 import pandas
@@ -17,34 +18,58 @@ def open_db(db_file):
     conn = sqlite3.connect(db_file)
     if db_check:
         init = '''create table if not exists JOURNAL(
-        ENTRY INTEGER PRIMARY KEY AUTOINCREMENT,
-        DATE VARCHAR(255) NOT NULL,
+        DATE TEXT PRIMARY KEY,
         RATING INTEGER NOT NULL,
-        WEATHER VARCHAR(255),
+        WEATHER TEXT,
         HEALTHY BIT DEFAULT 1,
         SLEEP INTEGER,
         STEPS INTEGER,
         WEIGHT INTEGER,
-        HIGHLIGHT VARCHAR(255));'''
+        HIGHLIGHT TEXT);'''
         conn.execute(init)
     return conn
+
+def insert_data(date, rating, weather, healthy, sleep, steps, weight, highlight):
+    try:
+        inserter = f'''INSERT INTO JOURNAL(
+        DATE,RATING,WEATHER,HEALTHY,SLEEP,STEPS,WEIGHT,HIGHLIGHT)
+        VALUES(
+        '{date}',{rating},'{weather}',{healthy},{sleep},{steps},{weight},'{highlight}');'''
+        conn.execute(inserter)
+        conn.commit()
+    except:
+        messagebox.showerror("Logging Failed", "There is already a log for today. Undo it first if you want to update.")
+    messagebox.showinfo("Logging Complete", "All data has been saved")
+
+def select_today():
+    def query_data():
+        query = f'''SELECT * FROM JOURNAL WHERE DATE = '{date.today()}';'''
+        rows = conn.execute(query).fetchall()
+        return rows
+    res = query_data()
+    print(res)
+    messagebox.showinfo("Result", res)
+
+def undo_today():
+    undotoday = f'''DELETE FROM JOURNAL WHERE DATE='{date.today()}';'''
+    conn.execute(undotoday)
+    messagebox.showinfo("Done", "Daily log cleared.")
 
 def clear_data():
     def truncate_table():
         global conn
         delete_table = '''DROP TABLE JOURNAL;'''
         init = '''create table if not exists JOURNAL(
-        ENTRY INTEGER PRIMARY KEY AUTOINCREMENT,
-        DATE VARCHAR(255) NOT NULL,
+        DATE TEXT PRIMARY KEY,
         RATING INTEGER NOT NULL,
-        WEATHER VARCHAR(255),
+        WEATHER TEXT,
         HEALTHY BIT DEFAULT 1,
         SLEEP INTEGER,
         STEPS INTEGER,
         WEIGHT INTEGER,
-        HIGHLIGHT VARCHAR(255));'''
-        cur.execute(delete_table)
-        cur.execute(init)
+        HIGHLIGHT TEXT);'''
+        conn.execute(delete_table)
+        conn.execute(init)
         cleardatapop.destroy()
     global cleardatapop
     cleardatapop = tkinter.Toplevel()
@@ -62,7 +87,7 @@ def getlocation():
 async def getweather(location):
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
         weather = await client.get(location)
-        return (f"{weather.current.temperature}, {weather.current.kind}")
+        return (f"{weather.current.temperature} and {weather.current.kind}")
 
 # Doodler Elements
 def doodler():
@@ -103,9 +128,8 @@ def paint(e):
 
 # Data Variables
 conn = open_db('Journal.sqlite')
-cur = conn.cursor()
 location = getlocation()
-weather = asyncio.run(getweather(location))
+weather = str(asyncio.run(getweather(location)))
 
 # GUI Setup
 master = tkinter.Tk()
@@ -117,29 +141,56 @@ style = ttk.Style()
 location_label = ttk.Label(text=location).pack()
 weather_label = ttk.Label(text=weather).pack()
 date_label = ttk.Label(text=(date.today())).pack()
-
 # Notebook setup for Organization
 notebook = ttk.Notebook(master, width=600, height=380)
 notebook.pack(pady=10, expand=True)
+# Logging Frame
 frame1 = ttk.Frame(notebook, width=600, height=380)
+rating_entry = tkinter.IntVar(frame1, 1)
+healthy_entry = tkinter.IntVar(frame1, 1)
+sleep_entry = tkinter.IntVar(frame1, 0)
+steps_entry = tkinter.IntVar(frame1, 0)
+weight_entry = tkinter.IntVar(frame1, 0)
+highlights_entry = tkinter.StringVar(frame1, ":3")
 frame1.pack(fill='both', expand=True)
+frame1.rowconfigure(7, weight=1)
+frame1.rowconfigure(8, weight=1)
+frame1.rowconfigure(9, weight=5)
+intro_label = ttk.Label(frame1, text="Complete your daily logging. Dont forget the doodle!").grid(row=0, column=1, columnspan=1)
+ratinglabel = ttk.Label(frame1, text="Enter mood rating 1-5").grid(column=0, row=1, sticky="W")
+ratingbox = ttk.Spinbox(frame1, from_=1, to=5, textvariable=rating_entry).grid(column=2, row=1, sticky="E")
+healthy_label = ttk.Label(frame1, text="Were you healthy today?").grid(column=0, row=2, sticky="W")
+health_check_yes = ttk.Radiobutton(frame1, text="Yes", variable=healthy_entry, value=1).grid(column=1, row=2, sticky="E")
+health_check_no = ttk.Radiobutton(frame1, text="No", variable=healthy_entry, value=0).grid(column=2, row=2, sticky="E")
+sleep_label = ttk.Label(frame1, text="How many hours did you sleep?").grid(column=0, row=3, sticky="W")
+sleep_entrybox = ttk.Entry(frame1, textvariable=sleep_entry).grid(column=2, row=3, columnspan=1, sticky="E")
+steps_label = ttk.Label(frame1, text="How many steps did you take?").grid(column=0, row=4, sticky="W")
+steps_entrybox = ttk.Entry(frame1, textvariable=steps_entry).grid(column=2, row=4, columnspan=1, sticky="E")
+weight_label = ttk.Label(frame1, text="What was your weight today?").grid(column=0, row=5, sticky="W")
+weight_entrybox = ttk.Entry(frame1, textvariable=weight_entry).grid(column=2, row=5, columnspan=1, sticky="E")
+highlights_label = ttk.Label(frame1, text="What was your daily highlight?").grid(column=0, row=6, sticky="W")
+Highlights_entry = ttk.Entry(frame1, textvariable=highlights_entry).grid(column=1, row=6, columnspan=2, sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S)
+doodle_button = ttk.Button(frame1, text="Doodle", command=doodler).grid(column=0, row=9, sticky="SW")
+log_button = ttk.Button(frame1, text="Log", command=lambda : insert_data(date.today(), rating_entry.get(), weather, healthy_entry.get(), sleep_entry.get(), steps_entry.get(), weight_entry.get(), highlights_entry.get())).grid(column=2, row=9, sticky="SE")
+# Review Frame
 frame2 = ttk.Frame(notebook, width=600, height=380)
 frame2.pack(fill='both', expand=True)
+review_button = ttk.Button(frame2, text="Review", command = lambda : select_today()).pack()
+search_button = ttk.Button(frame2, text="Search").pack()
+# Charting Frame
 frame3 = ttk.Frame(notebook, width=600, height=380)
 frame3.pack(fill='both', expand=True)
+graph_button = ttk.Button(frame3, text="Graph").pack()
+# Tools Frame
 frame4 = ttk.Frame(notebook, width=600, height=380)
 frame4.pack(fill='both', expand=True)
+undotoday_button = ttk.Button(frame4, text="Undo Today", command=undo_today).pack()
+cleardata_button = ttk.Button(frame4, text="Clear All Data", command=clear_data).pack()
+exportdata_button = ttk.Button(frame4, text="Export All Data").pack()
+# Adding frames to notebook
 notebook.add(frame1, text='Log Entry')
 notebook.add(frame2, text='Review Entries')
 notebook.add(frame3, text='Charting')
 notebook.add(frame4, text='Tools')
-
-# Buttons
-log_button = ttk.Button(frame1, text="Log").pack()
-doodle_button = ttk.Button(frame1, text="Doodle", command=doodler).pack()
-review_button = ttk.Button(frame2, text="Review").pack()
-search_button = ttk.Button(frame2, text="Search").pack()
-graph_button = ttk.Button(frame3, text="Graph").pack()
-cleardata_button = ttk.Button(frame4, text="Clear Data", command=clear_data).pack()
 
 master.mainloop()
